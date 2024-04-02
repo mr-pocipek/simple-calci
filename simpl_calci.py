ba670dd366002103d8a29792ca385622b3b877c3
Hithesh2k3 <hithesh2k3@gmail.com>
2024-04-02 14:36:11 +0530

    added remainder/modulo function

--- simpl_calci.py
+++ simpl_calci.py
@@ -20,32 +20,10 @@ def operation(a,b,op):
     print(a-b);
   elif (op=="mul"):
     print(a*b);
+  elif (op=="rem"):
+    print(a%b);
   elif (op=="div"):
     print(a/b);
 
 main()
 
-a= input("Enter your Name:");
-b= input("Enter the time_24hr format:");
-h, m = map(int, b.split(':'))
-if (h<12 and m!=0):
-    print("Good Morning");
-elif (h==12 and m==0):
-    print("Good Afternoon");
-else:
-    print("Good Evening");
-
-def greet(name, hour, minute):
-    if hour < 12 or (hour == 12 and minute == 0):
-        print(f"Good morning, {name}!")
-    else:
-        print(f"Good evening, {name}!")
-
-def main():
-    name = input("Enter your name: ")
-    time_input = input("Enter the time in HH:MM format (24-hour clock): ")
-    hour, minute = map(int, time_input.split(':'))
-    greet(name, hour, minute)
-
-if __name__ == "__main__":
-    main()
